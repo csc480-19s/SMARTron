86a8245542ace0150b8d63274f689716dbf7bbbd
Nicholas Esposito <nesposi3@oswego.edu>
2019-03-25 13:03:26 -0400

    Edited style of results header

--- smartron/src/components/Results.js
+++ smartron/src/components/Results.js
@@ -19,7 +19,7 @@ class Results extends Component {
     render() {
         return (
             <div>
-                <div style={{background: '#f2f2f2'}}><h1 style={{ paddingLeft: '50px',color: '#003C60'}}>SMART<span style={{color:'#00CCBC'}}>RON</span></h1></div>
+                <div style={{background: '#f2f2f2'}}><h1 style={{ paddingLeft: '50px',color: '#003C60', fontFamily:"Helvetica Neue"}}>SMART<span style={{color:'#00CCBC', fontFamily:"Helvetica Neue"}}>RON</span></h1></div>
                 <Link to={'/home'} style={{ textDecoration: 'none', color: '#003C60'}}>&lt;Back to Dashboard</Link><h1 style={{ color: '#00ccbc', paddingLeft: '50px' }}>Results</h1>
                 <p style={{ color: '#00ccbc', paddingLeft: '50px' }}>Final - 11250 05152018 13:06</p>
                 <Center><Tabs /></Center>
