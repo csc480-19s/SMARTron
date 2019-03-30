15e47aebd7f528b60ebcc2bc7a47851521e5d2cd
Jondn Tryniski <jtrynisk@oswego.edu>
2019-03-29 20:54:26 -0400

    updated project structure

---
+++ src/main/java/SMARTron/Engine/PseudoStudent.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SMARTron.Engine;
+
+public class PseudoStudent {
+    String[][] multi;
+    String[] array;
+    
+    public PseudoStudent(String[][] a, String[] s){
+        this.multi = a;
+        this.array = s;
+    }
+    
+}
