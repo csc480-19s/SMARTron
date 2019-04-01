3eaaf2000ea0925fd551c105934a86abcbf64792
rango <nneopane@oswego.edu>
2019-04-01 00:54:02 -0400

    Reverting to fix merge conflict

--- smartron/src/components/AnswerKey.js
+++ smartron/src/components/AnswerKey.js
@@ -6,20 +6,15 @@ constructor(){
   super();
   this.state = {
     answerKeys: [],
-    keyOptions: ["A", "B", "C", "D", "E"],
+    list: ["A", "B", "C", "D", "E"],
     chkbox: false,
-    updateAnswerKey: [],
-    copyOfAnswerKey: [],
-    makeCopy: true
   };
   this.handleClick = this.handleClick.bind(this);
   this.handleSubmit = this.handleSubmit.bind(this);
-  this.copyAnswerKey = this.copyAnswerKey.bind(this);
-  this.postAnswerKey = this.postAnswerKey.bind(this);
 }
 
 componentDidMount(){
-  fetch('https://642b4d9c-37ba-432e-823a-30c17de3fb13.mock.pstmn.io/answerKey')
+  fetch('./answerkey.json')
   .then(response => response.json())
   .then(result => {
     const keys = result.map(item => {
@@ -31,55 +26,19 @@ componentDidMount(){
   });
 }
 
-copyAnswerKey(){
-  let dupeAnswerKey = JSON.parse(JSON.stringify(this.state.answerKeys));
-  this.setState({
-    makeCopy:false,
-    copyOfAnswerKey:dupeAnswerKey
-  });
-}
-
-handleClick(item, key){
-  if(this.state.makeCopy){
-    this.copyAnswerKey();
+handleClick(item, index){
+  var i = item.answerKey.indexOf(index);
+  if(item.answerKey.includes(index)){
+    item.answerKey.splice(i,1);
+  }else{
+    item.answerKey.push(index);
   }
-  var index = item.answerKey.indexOf(key);
-    if(item.answerKey.includes(key)){
-      item.answerKey.splice(index,1);
-    }else{
-      item.answerKey.push(key);
-    }
-    console.log();
-  if(this.state.updateAnswerKey.length === 0){
-    this.state.updateAnswerKey.push(item);
-  }
-
-  let updateList = this.state.updateAnswerKey;
-  updateList = updateList.filter(function( obj ){
-  return obj.questionId !== item.questionId});
-
-  let anserKeyObj = this.state.copyOfAnswerKey
-    .find(obj => obj.questionId === item.questionId);
-    item.answerKey.sort();
-
-  if(JSON.stringify(item) !== JSON.stringify(anserKeyObj)){
-    updateList.push(item);
-  }
-  this.setState({
-    updateAnswerKey:updateList
-  });
 }
 
 handleSubmit(e){
   console.log("handle on submit here")
-  console.log(this.state.copyOfAnswerKey);
-  console.log(this.state.updateAnswerKey);
-  this.postAnswerKey();
 }
 
-postAnswerKey(){
-  //send answerkey to backend
-}
   render(){
     return (
       <div>
@@ -91,13 +50,13 @@ postAnswerKey(){
           <h1 className = "examName" align = "center">Exam 101</h1>
             <div className = "items">
               {this.state.answerKeys.map( item => (
-                <div align = "center" className ={item.answerKey.length !== 0? "item": "itemEmpty"}>
+                <div className = "item">
                   <div key ={item.questionId}>
                     <div className="keyBox">
                       <span className="questionId">
                         {item.questionId}
                       </span>
-                        {this.state.keyOptions.map(k =>(
+                        {this.state.list.map(k =>(
                           <span>
                             <input
                               type="checkbox"
