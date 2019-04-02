dab1d48fe0b24ec5c74096e10c4eff61a1ce29ff
Jondn Tryniski <jtrynisk@oswego.edu>
2019-04-02 07:57:12 -0400

    more answerkey tests

--- src/test/groovy/SMARTron/AnswerKeyDaoTest.groovy
+++ src/test/groovy/SMARTron/AnswerKeyDaoTest.groovy
@@ -21,6 +21,7 @@ class AnswerKeyDaoTest extends Specification{
     def "correct addition of an answerkey"() {
 
         when:
+        //This simply ensures that the exam is not already in the DB
         akd.deleteAnswerKey('midterm')
         akd.addAnswerKey('midterm', 'MATT', 'a,b,c,d')
 
@@ -85,6 +86,7 @@ class AnswerKeyDaoTest extends Specification{
 
 
     //Should throw Exception based on the code, but returns an empty list
+    //The exception should be thrown from select
     @Ignore
     def "test deletion of an exam"() {
 
@@ -94,6 +96,61 @@ class AnswerKeyDaoTest extends Specification{
         and:
         akd.deleteAnswerKey("Midterm")
         akd.selectAnswerKey('Midterm', 'MATT')
+
+        then:
+        thrown(Exception)
+
+    }
+
+    //Another failure of throwing an exception...le sad
+    @Ignore
+    def "test error throwing of a delete of an exam"() {
+
+        when:
+        akd.deleteAnswerKey("Midterm")
+
+        then:
+        thrown(Exception)
+
+    }
+
+    def "test addition of an updated answer key"() {
+
+        when:
+        akd.addAnswerKey('Midterm', 'MATT', 'a,b,c')
+        akd.addUpdatedAnswerKey('Midterm', 'a,b,c,d,e,f')
+
+        then:
+        def result = akd.selectUpdatedAnswerKey('Midterm', 'MATT')
+        result == ['a,b,c,d,e,f']
+        akd.deleteAnswerKey('Midterm')
+
+    }
+
+    //This should throw an error as that test doesnt exist, midterm is null through
+    //the selectUpdated. the non existent test ends up being an empty list
+    @Ignore
+    def "test thrown exception of an updated answer key"() {
+
+        when:
+        akd.addAnswerKey("Midterm", "MATT", 'a,b,c')
+        akd.addUpdatedAnswerKey("Halfterm", 'a,b,c,d')
+
+        then:
+        thrown(Exception)
+        def wrongResult = akd.selectUpdatedAnswerKey('Halfterm', 'MATT')
+        wrongResult == ['a,b,c,d']
+        akd.deleteAnswerKey("Midterm")
+
+    }
+
+    //What I am learning is ain't nothing throwing exceptions that should... :(
+    @Ignore
+    def'test throwing an exception for selecting an answer key that doesnt exist'() {
+
+        when:
+        akd.deleteAnswerKey("Midterm")
+        akd.selectAnswerKey("Midterm", "MATT")
 
         then:
         thrown(Exception)
