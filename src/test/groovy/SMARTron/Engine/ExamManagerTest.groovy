536f0f7ea2e587f86cd9606ebb973f797e7539bf
Tim Gass <tgass@oswego.edu>
2019-04-08 13:35:10 -0400

    adding other test files

---
+++ src/test/groovy/SMARTron/Engine/ExamManagerTest.groovy
@@ -0,0 +1,6 @@
+package SMARTron.Engine
+
+import spock.lang.Specification
+
+class ExamManagerTest extends Specification{
+}
---
+++ src/test/groovy/SMARTron/Engine/LetterConverterTest.groovy
@@ -0,0 +1,6 @@
+package SMARTron.Engine
+
+import spock.lang.Specification
+
+class LetterConverterTest extends Specification{
+}
