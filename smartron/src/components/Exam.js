e2f191362c8ebc93883a6aca71e806332159cd04
Nicholas Esposito <nesposi3@oswego.edu>
2019-04-22 14:35:54 -0400

    Starting to add ability to change exam name

--- smartron/src/components/Exam.js
+++ smartron/src/components/Exam.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
 import alert from '../assets/alert.png';
 import '../css/exam.css';
+import Popup from "reactjs-popup";
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
 class Exam extends Component{
@@ -10,12 +12,16 @@ class Exam extends Component{
         this.state = {
             title1: props.text,
             problem: props.problem,
-            exid: props.scanCode
+            exid: props.scanCode,
+            newName:""
         }
         console.log(props.problem)
 
         this.navResults = this.navResults.bind(this);
         this.navAnswerKey = this.navAnswerKey.bind(this);
+        this.handleName = this.handleName.bind(this);
+        this.suppressEnter = this.suppressEnter.bind(this);
+        this.resetNewNames = this.resetNewNames.bind(this);
     }
 
 
@@ -23,21 +29,64 @@ class Exam extends Component{
 
     navResults(){
         // createBrowserHistory().push("/results");
-        this.props.history.push({pathname:"/results",state:{id:this.state.id, text:this.state.title1 ,loginName:this.props.loginName,exams:this.props.list,email:this.props.email}});
+        this.props.history.push({pathname:"/results",state:{id:this.state.exid, text:this.state.title1 ,loginName:this.props.loginName,exams:this.props.list,email:this.props.email}});
     }
 
     navAnswerKey(){
-      this.props.history.push({pathname:"/answerKey",state:{id:this.state.id,text:this.state.title1, loginName:this.props.loginName,exams:this.props.list,email:this.props.email}});
+      this.props.history.push({pathname:"/answerKey",state:{id:this.state.exid,text:this.state.title1, loginName:this.props.loginName,exams:this.props.list,email:this.props.email}});
+    }
+    handleName(event){
+        this.setState({newName: event.target.value});
+
+    }
+    suppressEnter(event){
+        if (event.which === 13) {
+            event.preventDefault();
+        }
+    }
+    resetNewNames(){
+        this.state.newName = ""
+        this.setState(this.state)
     }
     render() {
         return(
             <div>
                 <div className={"exam"}>
-                    {this.state.title1}
-                    <a className={"editName"}><FontAwesomeIcon icon={faPencilAlt}/></a>
+
+                    <a className={"editName"}>{this.state.title1} </a>
+                    <Popup onClose={this.resetNewNames} modal trigger={<FontAwesomeIcon icon={faPencilAlt}/>}>
+                        {close =>
+                            <div className={"changeName"}>
+                                <h1>Change name of test {this.state.title1}</h1>
+                                <form onKeyPress={this.suppressEnter}>
+                                    <label> Enter new name of test: <input name={"newName"} value={this.state.newName}
+                                                                           onChange={this.handleName}/></label> <br/>
+                                </form>
+                                <button onClick={()=>{
+                                    this.resetNewNames()
+                                    close()
+                                }
+                                }>Submit</button>
+                                <button onClick={()=>{
+                                    this.resetNewNames()
+                                    close()
+
+                                }
+                                }>
+                                Cancel</button>
+
+                            </div>
+
+                        }
+
+                    </Popup>
+
+
+
+                    <>    Scan Code: {this.props.scanCode}</>
+
                     <button onClick={this.navResults}>View Results</button>
                     <button onClick={this.navAnswerKey}>Edit Answer Key</button>
-
                     {this.props.problem ? <img className={"alert"} src={alert} height={25} /> : null}
 
                 </div>
--- smartron/src/components/Home.js
+++ smartron/src/components/Home.js
@@ -24,6 +24,7 @@ class Home extends Component {
         this.handleName = this.handleName.bind(this)
         this.handleNum = this.handleNum.bind(this)
         this.sort = this.sort.bind(this)
+        this.resetNewTest = this.resetNewTest.bind(this)
 
     }
     componentDidMount() {
@@ -63,7 +64,7 @@ class Home extends Component {
         var id = ""
         var poss = "BCDFGHJKLMNPQRSTVWXYZ"
         for (let i = 0; i < 5; i++) {
-            id += poss.charAt(Math.floor(Math.random()*25))
+            id += poss.charAt(Math.floor(Math.random()*20))
         }
         this.state.random = id
         this.setState(this.state)
@@ -79,13 +80,18 @@ class Home extends Component {
         this.state.swap = !tmp
         this.setState(this.state)
     }
+    resetNewTest(){
+        this.state.newName = ""
+        this.state.newNum = ""
+        this.setState(this.state)
+    }
 
     render() {
         return (
             <div>
                 <h1 className={"welcome"}> Welcome, {this.props.location.state.loginName}</h1>
                 <div className={"buttons"}>
-                    <Popup  onClose={()=>{this.setState(this.state)}} onOpen={this.generateCode} modal trigger={<button   className={"scanButton"}>New Test Scan</button>}>
+                    <Popup  onClose={this.resetNewTest} onOpen={this.generateCode} modal trigger={<button   className={"scanButton"}>New Test Scan</button>}>
                         {close =>
                             <div className={"modal"}>
 
@@ -113,7 +119,8 @@ class Home extends Component {
                                         fetch(`http://pi.cs.oswego.edu:13126/exam?email=${this.props.location.state.email}&id=${this.state.random}&name=${this.state.newName}`,{
                                             method: 'post'
                                         })
-                                        this.state.exams.push(<Exam problem={false} loginName={this.props.location.state.loginName} email={this.props.location.state.email} list={[]} text={this.state.newName} id={this.state.random} numQuest={this.state.newNum} history={this.props.history}/>); this.setState(this.state)
+                                        this.state.exams.push(<Exam problem={false} loginName={this.props.location.state.loginName} scanCode={this.state.random} email={this.props.location.state.email} list={[]} text={this.state.newName} id={this.state.random} numQuest={this.state.newNum} history={this.props.history}/>); this.setState(this.state)
+                                        this.resetNewTest()
                                         close()
                                     }}>Ok
                                     </button>
