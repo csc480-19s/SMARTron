be2c4fc3d1abb32e2d576de4679b7412694ba8fe
Nicholas Esposito <nesposi3@oswego.edu>
2019-04-22 17:03:19 -0400

    Updated logout button to actually log out of google

--- smartron/src/components/Header.js
+++ smartron/src/components/Header.js
@@ -3,6 +3,9 @@ import logo from '../assets/logo.svg';
 import '../css/App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPowerOff } from '@fortawesome/free-solid-svg-icons'
+import { GoogleLogout } from 'react-google-login';
+
+
 class Header extends Component{
     constructor(props) {
         super(props);
@@ -13,7 +16,7 @@ class Header extends Component{
     return(
       <header className={"topBar"}>
           <h1 className={"leftA"}>SMART</h1><h1 className={"leftB"}>RON</h1>
-          <button className={"logout"} onClick={()=>{this.props.history.push("/")}}><FontAwesomeIcon icon={faPowerOff} size={"lg"}/></button>
+          <GoogleLogout clientId={"121480712018-0f6fb2fh7kbms55bno97g3hiju99n8oo.apps.googleusercontent.com"} buttonText={"Logout"} onLogoutSuccess={()=>{this.props.history.push("/")}} render={ renderProps =>(<button className={"logout"} onClick={()=>{this.props.history.push("/")}}><FontAwesomeIcon icon={faPowerOff} size={"lg"}/></button>)}/>
           <h1 className={"right"}>{this.props.email} </h1>
 
       </header>
