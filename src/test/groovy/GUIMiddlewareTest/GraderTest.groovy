16ea369929585e7ac9d247cc43fe2d62c653ad97
Jondn Tryniski <jtrynisk@oswego.edu>
2019-05-06 12:08:13 -0400

    finished grader test

--- src/test/groovy/GUIMiddlewareTest/GraderTest.groovy
+++ src/test/groovy/GUIMiddlewareTest/GraderTest.groovy
@@ -85,4 +85,67 @@ class GraderTest extends Specification {
         then:
         thrown(Exception)
     }
+
+    def "test a run of grader with a non 100 or 0 score"() {
+        when:
+        def testAnswers = [['3', '1', '2', '2', '4', '0'], ['3', '2', '4', '2', '1', '3']]
+        def testStudent = ['10', '15', '14', '4', '14', '0', '20', '18', '25', '14', '9', '19',
+                           '11', '9', '0', '13', '0', '0', '0', '0', '13', '1', '1', '1', '7',
+                           '9', '2', '8', '0', '3', '0', '4', '3', '9', '7', '4', '1', '2', '3', '4', '5', '6', '1']
+        def test = sc.makeStudent(testAnswers as String[][], testStudent as String[])
+        def testList = [test]
+        def keyData = new String[46]
+        for (def i = 0; i < 46; i++) {
+            keyData[i] = "1"
+        }
+        def key = sc.makeStudent([['1', '1', '3', '2', '4', '1'], ['3', '2', '2', '2', '1', '3']] as String[][], keyData as String[])
+        def gradedList = g.getGrades(testList, key)
+
+        then:
+        gradedList.get(0) == 66.66667 as float
+    }
+
+    def "test a run of grader with a 100, a 0, and a calculated grade"() {
+        when:
+        def testAnswers = [['3', '1', '2', '2', '4', '0'], ['3', '2', '4', '2', '1', '3']]
+        def testStudent = ['10', '15', '14', '4', '14', '0', '20', '18', '25', '14', '9', '19',
+                           '11', '9', '0', '13', '0', '0', '0', '0', '13', '1', '1', '1', '7',
+                           '9', '2', '8', '0', '3', '0', '4', '3', '9', '7', '4', '1', '2', '3', '4', '5', '6', '1']
+        def test = sc.makeStudent(testAnswers as String[][], testStudent as String[])
+        def testAnswers100 = [['1', '1', '3', '2', '4', '1'], ['3', '2', '2', '2', '1', '3']]
+        def testAnswers0 = [['0', '0', '0', '0', '0', '0', ], ['0', '0', '0', '0', '0', '0', ]]
+        def test100 = sc.makeStudent(testAnswers100 as String[][], testStudent as String[])
+        def test0 = sc.makeStudent(testAnswers0 as String[][], testStudent as String[])
+        def testList = [test, test100, test0]
+        def keyData = new String[46]
+        for (def i = 0; i < 46; i++) {
+            keyData[i] = "1"
+        }
+        def key = sc.makeStudent([['1', '1', '3', '2', '4', '1'], ['3', '2', '2', '2', '1', '3']] as String[][], keyData as String[])
+        def gradedList = g.getGrades(testList, key)
+
+        then:
+        gradedList.get(0) == 66.66667 as float
+        gradedList.get(1) == 100
+        gradedList.get(2) == 0
+    }
+
+    def "test of a student choosing multiple answers"() {
+        when:
+        def testAnswers = [['3', '1 1', '2'], ['3', '2', '4']]
+        def testStudent = ['10', '15', '14', '4', '14', '0', '20', '18', '25', '14', '9', '19',
+                           '11', '9', '0', '13', '0', '0', '0', '0', '13', '1', '1', '1', '7',
+                           '9', '2', '8', '0', '3', '0', '4', '3', '9', '7', '4', '1', '2', '3', '4', '5', '6', '1']
+        def test = sc.makeStudent(testAnswers as String[][], testStudent as String[])
+        def testList = [test]
+        def keyData = new String[46]
+        for (def i = 0; i < 46; i++) {
+            keyData[i] = "1"
+        }
+        def key = sc.makeStudent([['3', '1', '2'], ['3', '2', '4']] as String[][], keyData as String[])
+        def gradedList = g.getGrades(testList, key)
+
+        then:
+        gradedList.get(0) == 83.33333 as float
+    }
 }
